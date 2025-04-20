cf8ddb3ece3eaac25e93e01a152a734d4d54c8b8
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-04-20 21:30:55 +0530

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/img/logo.svg';
 import './App.css'
+import {NavBar}  from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <NavBar />
+    </div>
   )
 }
 
