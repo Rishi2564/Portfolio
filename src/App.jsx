c0a0dc84377b54ab9df1716e77dc6c3eeb91fb9f
Rishi2564 <161162489+Rishi2564@users.noreply.github.com>
2025-04-20 22:05:30 +0530

    added all the things in APp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import logo from './assets/img/logo.svg';
 import './App.css'
-import {NavBar}  from './components/Navbar'
+import {NavBar}  from './components/Navbar';
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +13,11 @@ function App() {
   return (
     <div className='App'>
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
